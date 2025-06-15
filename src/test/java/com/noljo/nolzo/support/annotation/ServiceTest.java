bcdf1199ee1e7263ae5c1876487e23d9e59bf1fa
hwan2-99 <thh1233123@gmail.com>
2025-06-15 21:38:26 +0900

    feat: 테스트용 어노테이션 구현 및 주석처리

---
+++ src/test/java/com/noljo/nolzo/support/annotation/ServiceTest.java
@@ -0,0 +1,23 @@
+//package com.noljo.nolzo.support.annotation;
+//
+//import java.lang.annotation.ElementType;
+//import java.lang.annotation.Retention;
+//import java.lang.annotation.RetentionPolicy;
+//import java.lang.annotation.Target;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@Target(ElementType.TYPE)
+//@ActiveProfiles("test")
+//@Retention(RetentionPolicy.RUNTIME)
+//@SpringBootTest(webEnvironment = WebEnvironment.NONE, classes = TestConfig.class)
+//@ExtendWith({DatabaseCleanerExtension.class})
+//@PropertySource("classpath:/application-test.yml")
+//public @interface ServiceTest {
+//}
+
+
+// 추후 추가예정
