1177197ff995114ad93606caf7347a63e2cb5d08
hwan2-99 <thh1233123@gmail.com>
2025-06-16 11:02:57 +0900

    refactor: 불필요한 주석 삭제

--- src/main/java/com/noljo/nolzo/global/config/JpaConfig.java
+++ src/main/java/com/noljo/nolzo/global/config/JpaConfig.java
@@ -2,7 +2,6 @@ package com.noljo.nolzo.global.config;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -12,10 +11,4 @@ public class JpaConfig {
 
     @PersistenceContext
     private EntityManager entityManager;
-
-//    @Bean
-//    public JPAQueryFactory jpaQueryFactory() {
-//        return new JPAQueryFactory(entityManager);
-//    }
-
 }
