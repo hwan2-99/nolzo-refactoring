d05077965cafb0d31f53940e53896f2d87347dbc
present0917 <guswo0329@gmail.com>
2025-06-17 21:24:38 +0900

    refactor: EventService 메서드분리

--- src/main/java/com/noljo/nolzo/event/service/EventService.java
+++ src/main/java/com/noljo/nolzo/event/service/EventService.java
@@ -1,7 +1,7 @@
 package com.noljo.nolzo.event.service;
 
-import com.noljo.nolzo.event.dto.EventRequestDto;
-import com.noljo.nolzo.event.dto.EventResponseDto;
+import com.noljo.nolzo.event.dto.EventRequest;
+import com.noljo.nolzo.event.dto.EventResponse;
 import com.noljo.nolzo.event.entity.Event;
 import com.noljo.nolzo.event.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,41 +17,21 @@ public class EventService {
 
     private final EventRepository eventRepository;
 
+    public Event getEvent(Long id){
+        return eventRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 이벤트가 존재하지 않습니다 id : " +id));
+    }
+
     @Transactional(readOnly = true)
-    public List<EventResponseDto> findAll() {
+    public List<EventResponse> findAll() {
         return eventRepository.findAll().stream()
-                .map(EventResponseDto::from)
+                .map(EventResponse::from)
                 .toList();
     }
 
-    @Transactional(readOnly = true)
-    public EventResponseDto findById(Long id) {
-        Event event = eventRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 이벤트가 존재하지 않습니다 id: " + id));
-
-        return EventResponseDto.from(event);
-    }
-
-
-    public EventResponseDto save(EventRequestDto dto) {
+    public EventResponse save(EventRequest dto) {
         Event saved = eventRepository.save(dto.toEntity());
-        return EventResponseDto.from(saved);
+        return EventResponse.from(saved);
     }
 
-    public EventResponseDto update(Long id, EventRequestDto dto) {
-        if (!eventRepository.existsById(id)) {
-            throw new IllegalArgumentException("해당 이벤트가 존재하지 않습니다 id: " + id);
-        }
 
-        Event updated = dto.toEntity(id);
-        Event saved = eventRepository.save(updated);
-        return EventResponseDto.from(saved);
-    }
-
-    public void delete(Long id) {
-        if (!eventRepository.existsById(id)) {
-            throw new IllegalArgumentException("삭제할 이벤트가 없습니다 id: " + id);
-        }
-        eventRepository.deleteById(id);
-    }
 }
