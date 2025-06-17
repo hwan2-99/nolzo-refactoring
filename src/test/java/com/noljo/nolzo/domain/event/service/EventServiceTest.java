79caa583d8eb196c00826da2ed6795c0b6d9451c
present0917 <guswo0329@gmail.com>
2025-06-17 21:26:25 +0900

    test: Event CRUD 테스트코드

--- src/test/java/com/noljo/nolzo/domain/event/service/EventServiceTest.java
+++ src/test/java/com/noljo/nolzo/domain/event/service/EventServiceTest.java
@@ -1,18 +1,14 @@
 package com.noljo.nolzo.domain.event.service;
 
-import com.noljo.nolzo.event.dto.EventRequestDto;
-import com.noljo.nolzo.event.dto.EventResponseDto;
-import com.noljo.nolzo.event.entity.Event;
+import com.noljo.nolzo.event.dto.EventRequest;
+import com.noljo.nolzo.event.dto.EventResponse;
 import com.noljo.nolzo.event.service.EventService;
 import com.noljo.nolzo.support.annotation.ServiceTest;
 import com.noljo.nolzo.support.fixture.EventFixture;
-import com.sun.jdi.request.EventRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 @ServiceTest
 class EventServiceTest {
 
@@ -21,16 +17,51 @@ class EventServiceTest {
 
     @Test
     void 이벤트를_저장할_수_있다() {
-        EventRequestDto dto = EventFixture.캣츠dto();
-        EventResponseDto response = eventService.save(dto);
+        EventRequest dto = EventFixture.캣츠dto();
+        EventResponse response = eventService.save(dto);
         Assertions.assertThat(response.getId()).isNotNull();
         Assertions.assertThat(response.getTitle()).isEqualTo("Cats");
     }
 
     @Test
     void 이벤트를_조회할_수_있다(){
-        EventRequestDto dto = EventFixture.캣츠dto();
-        EventResponseDto response = eventService.save(dto);
+        EventRequest dto = EventFixture.캣츠dto();
+        EventResponse response = eventService.save(dto);
         Assertions.assertThat(response.getId()).isEqualTo(eventService.findAll().get(0).getId());
+    }
+
+    @Test
+    void 개별_이벤트를_조회할_수_있다(){
+
+        EventRequest dto = EventFixture.캣츠dto();
+
+        EventResponse response = eventService.save(dto);
+        Long id = response.getId();
+
+        Assertions.assertThat("Cats").isEqualTo(eventService.findById(id).getTitle());
+    }
+
+    @Test
+    void 이벤트를_삭제할_수_있다(){
+        EventRequest dto = EventFixture.캣츠dto();
+        EventResponse response = eventService.save(dto);
+        Long id = response.getId();
+
+        eventService.delete(id);
+
+        Assertions.assertThatThrownBy(()->eventService.findById(id)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이벤트를_갱신할_수_있다(){
+        EventRequest dtoCats = EventFixture.캣츠dto();
+        EventRequest dtoHam = EventFixture.햄릿dto();
+        EventResponse response = eventService.save(dtoCats);
+        Long id = response.getId();
+
+        EventResponse updatedResponse = eventService.update(id,dtoHam);
+
+        Assertions.assertThat(id).isEqualTo(updatedResponse.getId());
+        Assertions.assertThat("Hamlet").isEqualTo(updatedResponse.getTitle());
     }
 }
--- src/test/java/com/noljo/nolzo/support/fixture/EventFixture.java
+++ src/test/java/com/noljo/nolzo/support/fixture/EventFixture.java
@@ -1,6 +1,6 @@
 package com.noljo.nolzo.support.fixture;
 
-import com.noljo.nolzo.event.dto.EventRequestDto;
+import com.noljo.nolzo.event.dto.EventRequest;
 import com.noljo.nolzo.event.entity.Event;
 import com.noljo.nolzo.event.entity.EventCategory;
 import java.time.LocalDate;
@@ -9,7 +9,11 @@ import lombok.Getter;
 @Getter
 public enum EventFixture {
     캣츠("Cats", "서울 예술의 전당", "세계적으로 유명한 뮤지컬, 고양이들의 이야기를 그린 작품입니다.",
-            LocalDate.of(2024, 3, 1), LocalDate.of(2024, 6, 30), EventCategory.CONCERT, 12, 5, 120);
+            LocalDate.of(2024, 3, 1), LocalDate.of(2024, 6, 30), EventCategory.CONCERT, 12, 5, 120)
+,
+    햄릿("Hamlet", "국립극장 해오름극장", "셰익스피어 4대 비극 중 하나인 ‘햄릿’의 현대적 재해석 공연입니다.", LocalDate.of(2025, 7, 1),
+        LocalDate.of(2025, 7, 15), EventCategory.CONCERT, 15, 4, 80);
+
     private String title;
     private String venue;
     private String description;
@@ -37,8 +41,8 @@ public enum EventFixture {
         return new Event(null, 캣츠.title, 캣츠.venue, 캣츠.description, 캣츠.startDate, 캣츠.endDate, 캣츠.eventCategory,
                 캣츠.ageLimit, 캣츠.rating, 캣츠.reviewCount);
     }
-    public static EventRequestDto 캣츠dto() {
-    return EventRequestDto.builder()
+    public static EventRequest 캣츠dto() {
+    return EventRequest.builder()
             .title(캣츠.title)
             .venue(캣츠.venue)
             .description(캣츠.description)
@@ -47,5 +51,16 @@ public enum EventFixture {
             .eventCategory(캣츠.eventCategory)
             .ageLimit(캣츠.ageLimit)
             .build();
-}
+    }
+    public static EventRequest 햄릿dto() {
+        return EventRequest.builder()
+                .title(햄릿.title)
+                .venue(햄릿.venue)
+                .description(햄릿.description)
+                .startDate(햄릿.startDate)
+                .endDate(햄릿.endDate)
+                .eventCategory(햄릿.eventCategory)
+                .ageLimit(햄릿.ageLimit)
+                .build();
+    }
 }
