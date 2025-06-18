cd858d90f2bd3c9b9dd3e9184a3aaccb79a8b39e
hwan2-99 <thh1233123@gmail.com>
2025-06-18 16:48:19 +0900

    feat: 테스트를 위한 픽스처 구현

--- src/test/java/com/noljo/nolzo/support/fixture/SeatFixture.java
+++ src/test/java/com/noljo/nolzo/support/fixture/SeatFixture.java
@@ -7,7 +7,12 @@ import lombok.Getter;
 
 @Getter
 public enum SeatFixture {
-    일반좌석("B열", 7, "일반", "1층", 12000, SeatStatus.CANCELED);
+    일반좌석("B열", 7, "일반", "1층", 12000, SeatStatus.AVAILABLE),
+    VIP좌석("A열", 1, "VIP", "2층", 50000, SeatStatus.RESERVED),
+    프리미엄좌석("C열", 5, "프리미엄", "1층", 30000, SeatStatus.AVAILABLE),
+    일반좌석2("D열", 10, "일반", "3층", 15000, SeatStatus.AVAILABLE),
+    스탠딩("S열", 1, "스탠딩", "입구", 10000, SeatStatus.AVAILABLE);
+
     private String rowName;
     private int seatNumber;
     private String seatSection;
@@ -26,7 +31,22 @@ public enum SeatFixture {
     }
 
     public static Seat 일반좌석(Event event) {
-        return new Seat(null, 일반좌석.rowName, 일반좌석.seatNumber, 일반좌석.seatSection, 일반좌석.floor, 일반좌석.price, 일반좌석.status,
-                event);
+        return new Seat(null, 일반좌석.rowName, 일반좌석.seatNumber, 일반좌석.seatSection, 일반좌석.floor, 일반좌석.price, 일반좌석.status, event);
+    }
+
+    public static Seat VIP좌석(Event event) {
+        return new Seat(null, VIP좌석.rowName, VIP좌석.seatNumber, VIP좌석.seatSection, VIP좌석.floor, VIP좌석.price, VIP좌석.status, event);
+    }
+
+    public static Seat 프리미엄좌석(Event event) {
+        return new Seat(null, 프리미엄좌석.rowName, 프리미엄좌석.seatNumber, 프리미엄좌석.seatSection, 프리미엄좌석.floor, 프리미엄좌석.price, 프리미엄좌석.status, event);
+    }
+
+    public static Seat 일반좌석2(Event event) {
+        return new Seat(null, 일반좌석2.rowName, 일반좌석2.seatNumber, 일반좌석2.seatSection, 일반좌석2.floor, 일반좌석2.price, 일반좌석2.status, event);
+    }
+
+    public static Seat 스탠딩(Event event) {
+        return new Seat(null, 스탠딩.rowName, 스탠딩.seatNumber, 스탠딩.seatSection, 스탠딩.floor, 스탠딩.price, 스탠딩.status, event);
     }
 }
