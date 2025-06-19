7bd224f95494f07b1ef421eec5562fa46c80c6d9
chanwon <clw8679@naver.com>
2025-06-19 20:45:38 +0900

    feat: 인메모리 방식 RefreshTokenRepository 구현

---
+++ src/main/java/com/noljo/nolzo/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.noljo.nolzo.auth.repository;
+
+public interface RefreshTokenRepository {
+    void save(Long memberId, String refreshToken);
+
+    String findByMemberId(Long memberId);
+
+    void deleteByMemberId(Long memberId);
+
+    boolean hasRefreshToken(Long memberId);
+}
