bed671df1ef8f7bc41d27a678ffbe202b20b3a2c
paka <wjd1357912@gmail.com>
2025-06-21 00:22:46 +0900

    feat :findTicketStatusUsedByMemberId 사용자 정의 쿼리 메서드 작성

--- src/main/java/com/noljo/nolzo/reservation/repository/ReservationRepository.java
+++ src/main/java/com/noljo/nolzo/reservation/repository/ReservationRepository.java
@@ -3,6 +3,8 @@ package com.noljo.nolzo.reservation.repository;
 import com.noljo.nolzo.reservation.entity.Reservation;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.webjars.NotFoundException;
 
@@ -16,4 +18,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     }
 
     List<Reservation> findReservationsByMemberId(Long memberId);
+
+    @Query("SELECT r FROM Reservation r JOIN r.tickets t WHERE r.member.id = :memberId AND t.status = 'USED'")
+    List<Reservation> findTicketStatusUsedByMemberId(@Param("memberId") Long memberId);
 }
