7af9c47e3c70d4d9be24dccc5b416a32f0c612a3
GARDEN\wjd13 <wjd1357912@gmail.com>
2025-06-21 17:11:06 +0900

    feat:예약 취소 목록 조회 api 구현

--- src/main/java/com/noljo/nolzo/reservation/controller/ReservationController.java
+++ src/main/java/com/noljo/nolzo/reservation/controller/ReservationController.java
@@ -33,4 +33,11 @@ public class ReservationController {
         return ResponseEntity.ok(reservationDetails);
     }
 
+    @GetMapping("/cancel")
+    public ResponseEntity<List<ReservationEventInfo>> getCancelReservations(@AuthenticationPrincipal CustomUserDetails userDetails){
+        Long memberId = userDetails.getMemberId();
+        List<ReservationEventInfo> reservationDetails = reservationService.findCancelReservations(memberId);
+        return  ResponseEntity.ok(reservationDetails);
+    }
+
 }
