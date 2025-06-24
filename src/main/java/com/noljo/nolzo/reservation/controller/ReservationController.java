d18015736a450667ac20cb89dbd8c526bee5e388
hwan2-99 <thh1233123@gmail.com>
2025-06-24 16:33:11 +0900

    feat: 예약 생성 컨트롤러 작성

--- src/main/java/com/noljo/nolzo/reservation/controller/ReservationController.java
+++ src/main/java/com/noljo/nolzo/reservation/controller/ReservationController.java
@@ -4,6 +4,8 @@ package com.noljo.nolzo.reservation.controller;
 import com.noljo.nolzo.auth.security.CustomUserDetails;
 import com.noljo.nolzo.reservation.dto.ReservationEventInfo;
 import com.noljo.nolzo.reservation.dto.EventDateTimeResponse;
+import com.noljo.nolzo.reservation.dto.ReservationRequest;
+import com.noljo.nolzo.reservation.dto.ReservationResponse;
 import com.noljo.nolzo.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,41 +23,51 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
+    @PostMapping
+    public ResponseEntity<ReservationResponse> create(@AuthenticationPrincipal CustomUserDetails user,
+                                                      @RequestBody ReservationRequest request) {
+        return ResponseEntity.ok(reservationService.create(user.getMemberId(), request));
+    }
+
     @PostMapping("/{eventId}")
-    public ResponseEntity<EventDateTimeResponse> chooseEventDateTime(@PathVariable Long eventId ,
+    public ResponseEntity<EventDateTimeResponse> chooseEventDateTime(@PathVariable Long eventId,
                                                                      @RequestParam LocalDate selectDate,
                                                                      @RequestParam LocalTime selectTime) {
 
         EventDateTimeResponse event = reservationService.readSelectedEventDateTime(eventId, selectDate, selectTime);
         return ResponseEntity.ok(event);
     }
-  
+
     @GetMapping
-    public ResponseEntity<List<ReservationEventInfo>> getReservations(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<List<ReservationEventInfo>> getReservations(
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
         Long memberId = userDetails.getMemberId();
         List<ReservationEventInfo> reservationDetails = reservationService.findReservations(memberId);
         return ResponseEntity.ok(reservationDetails);
     }
 
     @GetMapping("/confirmed")
-    public ResponseEntity<List<ReservationEventInfo>> getReservationConfirmed(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<List<ReservationEventInfo>> getReservationConfirmed(
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
         Long memberId = userDetails.getMemberId();
         List<ReservationEventInfo> reservationDetails = reservationService.findReservationsConfirmed(memberId);
-        return  ResponseEntity.ok(reservationDetails);
+        return ResponseEntity.ok(reservationDetails);
 
     }
 
     @GetMapping("/used")
-    public ResponseEntity<List<ReservationEventInfo>> getTicketsUsed(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<List<ReservationEventInfo>> getTicketsUsed(
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
         Long memberId = userDetails.getMemberId();
         List<ReservationEventInfo> reservationDetails = reservationService.findTicketsUsed(memberId);
         return ResponseEntity.ok(reservationDetails);
     }
-  
+
     @GetMapping("/cancel")
-    public ResponseEntity<List<ReservationEventInfo>> getCancelReservations(@AuthenticationPrincipal CustomUserDetails userDetails){
+    public ResponseEntity<List<ReservationEventInfo>> getCancelReservations(
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
         Long memberId = userDetails.getMemberId();
         List<ReservationEventInfo> reservationDetails = reservationService.findCancelReservations(memberId);
-        return  ResponseEntity.ok(reservationDetails);
+        return ResponseEntity.ok(reservationDetails);
     }
 }
