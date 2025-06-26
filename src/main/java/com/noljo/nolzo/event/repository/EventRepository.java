b32a2a5a894b4ae3c49c164743a23b73f2a2e311
hwan2-99 <thh1233123@gmail.com>
2025-06-26 15:10:25 +0900

    style: 미사용 선언문 제거

--- src/main/java/com/noljo/nolzo/event/repository/EventRepository.java
+++ src/main/java/com/noljo/nolzo/event/repository/EventRepository.java
@@ -3,8 +3,6 @@ package com.noljo.nolzo.event.repository;
 import com.noljo.nolzo.event.entity.Event;
 import com.noljo.nolzo.event.entity.EventCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
