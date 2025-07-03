30f400b260ad14360801a9bb050c8d5573b426ec
Bogeun Kim <mmmjjjkim@naver.com>
2025-07-03 14:48:10 +0900

    feat: 리뷰 생성 로직 구현

--- src/main/java/com/noljo/nolzo/review/controller/ReviewController.java
+++ src/main/java/com/noljo/nolzo/review/controller/ReviewController.java
@@ -1,8 +1,16 @@
 package com.noljo.nolzo.review.controller;
 
+import com.noljo.nolzo.auth.security.CustomUserDetails;
+import com.noljo.nolzo.review.dto.request.ReviewCreateRequest;
+import com.noljo.nolzo.review.dto.response.ReviewResponse;
 import com.noljo.nolzo.review.service.ReviewService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +18,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/reviews")
 public class ReviewController {
     private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<ReviewResponse> createReview(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody ReviewCreateRequest request) {
+        ReviewResponse response = reviewService.create(user.getMemberId(), request);
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/noljo/nolzo/review/service/ReviewService.java
+++ src/main/java/com/noljo/nolzo/review/service/ReviewService.java
@@ -1,5 +1,12 @@
 package com.noljo.nolzo.review.service;
 
+import com.noljo.nolzo.event.entity.Event;
+import com.noljo.nolzo.event.repository.EventRepository;
+import com.noljo.nolzo.member.entity.Member;
+import com.noljo.nolzo.member.repository.MemberRepository;
+import com.noljo.nolzo.review.dto.request.ReviewCreateRequest;
+import com.noljo.nolzo.review.dto.response.ReviewResponse;
+import com.noljo.nolzo.review.entity.Review;
 import com.noljo.nolzo.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +17,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+    private final EventRepository eventRepository;
+    private final MemberRepository memberRepository;
+
+    public ReviewResponse create(Long memberId, ReviewCreateRequest request) {
+        Member member = memberRepository.getOrThrow(memberId);
+
+        Event event = eventRepository.findById(request.eventId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이벤트입니다."));
+
+        Review review = new Review(request.content(), request.rating(), event, member);
+
+        return ReviewResponse.from(reviewRepository.save(review));
+    }
 }
