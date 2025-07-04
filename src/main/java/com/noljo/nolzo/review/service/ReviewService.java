2bde2247b3ae3f507410116500bf41668f475962
Bogeun Kim <mmmjjjkim@naver.com>
2025-07-04 16:30:45 +0900

    feat: 관람 완료되지 않은 이벤트에 대해 리뷰 작성 시 예외 처리 로직 추가

--- src/main/java/com/noljo/nolzo/review/service/ReviewService.java
+++ src/main/java/com/noljo/nolzo/review/service/ReviewService.java
@@ -4,6 +4,7 @@ import com.noljo.nolzo.event.entity.Event;
 import com.noljo.nolzo.event.repository.EventRepository;
 import com.noljo.nolzo.member.entity.Member;
 import com.noljo.nolzo.member.repository.MemberRepository;
+import com.noljo.nolzo.reservation.repository.ReservationRepository;
 import com.noljo.nolzo.review.dto.request.ReviewCreateRequest;
 import com.noljo.nolzo.review.dto.response.ReviewResponse;
 import com.noljo.nolzo.review.entity.Review;
@@ -19,13 +20,33 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final EventRepository eventRepository;
     private final MemberRepository memberRepository;
+    private final ReservationRepository reservationRepository;
 
     public ReviewResponse create(Long memberId, ReviewCreateRequest request) {
         Member member = memberRepository.getOrThrow(memberId);
         Event event = eventRepository.getOrThrow(request.eventId());
 
-        Review review = new Review(request.content(), request.rating(), event, member);
+        validateEventParticipation(memberId, request.eventId());
+        validateAlreadyReviewed(memberId, request.eventId());
 
+        Review review = new Review(request.content(), request.rating(), event, member);
         return ReviewResponse.from(reviewRepository.save(review));
+    }
+
+    private void validateEventParticipation(Long memberId, Long eventId) {
+        boolean isUsed = reservationRepository.findTicketStatusUsedByMemberId(memberId).stream()
+                .anyMatch(reservation -> reservation.getTickets().stream()
+                        .anyMatch(ticket -> ticket.getSeat().getSchedule().getEvent().getId().equals(eventId))
+                );
+        if (!isUsed) {
+            throw new IllegalStateException("리뷰는 관람 완료된 이벤트에 대해서만 작성할 수 있습니다.");
+        }
+    }
+
+    private void validateAlreadyReviewed(Long memberId, Long eventId) {
+        boolean isAlreadyReviewed = reviewRepository.findByMemberIdAndEventId(memberId, eventId).isPresent();
+        if (isAlreadyReviewed) {
+            throw new IllegalStateException("이미 해당 이벤트에 대한 리뷰를 작성하였습니다.");
+        }
     }
 }
