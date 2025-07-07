f77e55a511c16bf355c2ba838635205ece6dc307
chanwon <clw8679@naver.com>
2025-07-07 17:14:24 +0900

    feat: embeddedRedis로 테스트 환경 분리

---
+++ src/test/java/com/noljo/nolzo/support/RedisServerExtension.java
@@ -0,0 +1,27 @@
+package com.noljo.nolzo.support;
+
+import java.io.IOException;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import redis.embedded.RedisServer;
+
+public class RedisServerExtension implements BeforeAllCallback, AfterAllCallback {
+    private static RedisServer redisServer;
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws IOException {
+        if (redisServer == null) {
+            redisServer = new RedisServer(63790);
+            redisServer.start();
+        }
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) throws IOException {
+        if (redisServer != null) {
+            redisServer.stop();
+            redisServer = null;
+        }
+    }
+}
--- src/test/java/com/noljo/nolzo/support/RedisTemplateTests.java
+++ src/test/java/com/noljo/nolzo/support/RedisTemplateTests.java
@@ -7,20 +7,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RedisTemplateTests {
-//    @Autowired
-//    private RedisTemplate<String, String> redisTemplate;
-//
-//    @Test
-//    @DisplayName("Redis 동작 테스트")
-//    void redisTemplateString() {
-//        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-//
-//        String key = "name";
-//        valueOperations.set(key, "NOLZO");
-//        String value = valueOperations.get(key);
-//        Assertions.assertEquals(value, "NOLZO");
-//    }
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Test
+    @DisplayName("Redis 동작 테스트")
+    void redisTemplateString() {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+
+        String key = "name";
+        valueOperations.set(key, "NOLZO");
+        String value = valueOperations.get(key);
+        Assertions.assertEquals(value, "NOLZO");
+    }
 }
