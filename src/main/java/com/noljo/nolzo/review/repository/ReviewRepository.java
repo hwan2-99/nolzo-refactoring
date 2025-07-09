2bff8fb70a922a5c62a0c84f7f5b79daf61c975d
chanwon <clw8679@naver.com>
2025-07-09 14:30:33 +0900

    feat: 평균 조회 쿼리문 추가

--- src/main/java/com/noljo/nolzo/review/repository/ReviewRepository.java
+++ src/main/java/com/noljo/nolzo/review/repository/ReviewRepository.java
@@ -1,9 +1,13 @@
 package com.noljo.nolzo.review.repository;
 
 import com.noljo.nolzo.review.entity.Review;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     default Review getOrThrow(Long id) {
@@ -15,5 +19,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Optional<Review> findByMemberIdAndEventId(Long memberId, Long eventId);
 
-    List<Review> findByEventId(Long eventId);
+    Page<Review> findPageByEventId(Long eventId, Pageable pageable);
+
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.event.id = :eventId")
+    Double getAverageByEventId(@Param("eventId") Long eventId);
 }
