3640122dad399d61c732183b146aed583f8c8c18
hwan2-99 <thh1233123@gmail.com>
2025-12-10 21:46:10 +0900

    refactor: db 커넥션 풀 사이즈 조절

--- k6/test.js
+++ k6/test.js
@@ -1,17 +1,16 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 20,
-    iterations: 20,
+    vus: 100, // 20명의 가상 사용자
+    iterations: 100, // 총 20번의 반복
     thresholds: {
-        http_req_duration: ['p(95)<500'],
+        http_req_duration: ['p(95)<500'], // 95%의 요청이 500ms 이하로 완료되도록 설정
     },
 };
 
 function login(userId) {
     const email = `${userId}@example.com`;
-
     const res = http.post('http://springboot:8080/auth/login', JSON.stringify({
         email: email,
         password: '1',
@@ -27,7 +26,6 @@ function login(userId) {
         console.log(`❗ Login failed for: ${email}`);
         return null;
     }
-
     return res.json('accessToken');
 }
 
@@ -36,13 +34,7 @@ function reserve(token) {
 
     const res = http.post('http://springboot:8080/reservations', JSON.stringify({
         eventId: 1,
-        seats: [ {
-            id: 1,
-            rowName: 'A',
-            seatNumber: 1,
-            seatSection: '1구역',
-            price: 150000,
-            status: 'AVAILABLE' } ],
+        seatIds: [1], // 좌석 ID 리스트 (여기서는 단일 좌석만)
     }), {
         headers: {
             'Content-Type': 'application/json',
@@ -58,5 +50,8 @@ function reserve(token) {
 export default function () {
     const userId = __VU;
     const token = login(userId);
-    reserve(token);
+
+    if (token) {
+        reserve(token);
+    }
 }
