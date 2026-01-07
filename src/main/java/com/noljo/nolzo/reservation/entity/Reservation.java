0c88cb0375fa4389365c4e4dbc903c09496c7072
hwan2-99 <thh1233123@gmail.com>
2026-01-07 21:59:17 +0900

    feat: 예약 엔티티에 멱등키를 관리하는 컬럼추가

--- src/main/java/com/noljo/nolzo/reservation/entity/Reservation.java
+++ src/main/java/com/noljo/nolzo/reservation/entity/Reservation.java
@@ -15,6 +15,12 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "reservation",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_reservation_idempotency_key", columnNames = {"idempotency_key"})
+        }
+)
 public class Reservation extends BaseEntity {
 
     @Id
@@ -29,6 +35,9 @@ public class Reservation extends BaseEntity {
 
     private String reservationNumber;
 
+    @Column(name = "idempotency_key", updatable = false, length = 64)
+    private String idempotencyKey;
+
     @OneToMany(mappedBy = "reservation", cascade = CascadeType.REMOVE)
     private List<Ticket> tickets = new ArrayList<>();
 
@@ -37,17 +46,23 @@ public class Reservation extends BaseEntity {
     private Member member;
 
     public Reservation(Long id, ReservationStatus status, int totalPrice, String reservationNumber,
-                       Member member) {
+                       String idempotencyKey, Member member) {
         this.id = id;
         this.status = status;
         this.totalPrice = totalPrice;
         this.reservationNumber = reservationNumber;
+        this.idempotencyKey = idempotencyKey;
         this.member = member;
     }
 
     public Reservation(ReservationStatus status, int totalPrice, String reservationNumber,
+                       Member member, String idempotencyKey) {
+        this(null, status, totalPrice, reservationNumber, idempotencyKey, member);
+    }
+
+    public Reservation(ReservationStatus status, int totalPrice, String reservationNumber,
                        Member member) {
-        this(null, status, totalPrice, reservationNumber, member);
+        this(null, status, totalPrice, reservationNumber, null, member);
     }
 
     public void updateStatus(ReservationStatus reservationStatus) {
